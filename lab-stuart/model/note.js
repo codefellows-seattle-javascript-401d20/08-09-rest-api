7223157d7f3ea2abe36bf2c108a6c7829314ea2e
Stuart Kershaw <stuartdkershaw@gmail.com>
2017-09-20 17:26:43 -0700

    Initial files / setup

---
+++ lab-stuart/model/note.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const uuid = require('uuid/v1');
+
+class Note {
+  constructor(options){
+    this.id = uuid();
+    this.timestamp = new Date();
+    this.title = options.title || '';
+    this.content = options.content || '';
+  }
+
+  toString(){
+    return this.title + '\n' + this.content;
+  }
+}
+
+module.exports = Note;
