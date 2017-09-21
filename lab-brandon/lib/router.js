f803ca145fe94b21f6afa31055f2aac8f9fcbe62
Brandon Arroyo <brandon.jarroyo@outlook.com>
2017-09-20 18:08:37 -0700

    class router demo up

--- lab-brandon/lib/router.js
+++ lab-brandon/lib/router.js
@@ -15,5 +15,32 @@ let routeHandlers = {
 
 module.exports = {
   get: (url, callback) => {
-    routeHandlers.GET[url] = callback
-  }
+    routeHandlers.GET[url] = callback;
+  },
+  post: (url, callback) => {
+    routeHandlers.POST[url] = callback;
+  },
+  put: (url, callback) => {
+    routeHandlers.PUT[url] = callback;
+  },
+  delete: (url, callback) => {
+    routeHandlers.DELETE[url] = callback;
+  },
+  route: (req, res) => {
+    requestParser(req)
+    .then(req => {
+      // find the handler
+      let handler = routeHandlers[req.method][req.url.pathname];
+      console.log('handler', req.url.pathname);
+      if(handler)
+        return handler(req, res);
+      res.writeHead(404);
+      res.end();
+    })
+    .catch(err => {
+      console.error('__REQUEST_ERROR__', err);
+      res.writeHead(400);
+      res.end();
+    });
+  },
+};
